2809b07b869fa919aed73e8bebb84c74a43729b1
Harm <mail@harmtemolder.nl>
2019-03-14 18:26:07 +0100

    Extracted imports from executables

---
+++ 1st-attempt/1st_attempt.py
@@ -0,0 +1,8 @@
+#!/Users/harmtemolder/miniconda3/envs/assistant-sdk-python/bin/python
+
+# Imports from executables:
+# from googlesamples.assistant.grpc.audio_helpers import main
+# from googlesamples.assistant.grpc.devicetool import main
+# from googlesamples.assistant.library.hotword import main
+# from googlesamples.assistant.grpc.pushtotalk import main
+
